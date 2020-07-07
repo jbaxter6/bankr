6823b1fa0917fa22b1adf65e3adda4adf0ec23c3
ianmcknnn <ianmcnnn@gmail.com>
2020-07-07 17:15:36 -0400

    added login page with form, added sessions controller and views folder

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+
+  def login
+
+  end
+
+  def process_login
+
+  end
+  
+  def logout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
     resources :transfers
   end
   resources :loans
+
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#process_login"
+  get "/logout", to: "sessions#logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
