35ef3b23458d77144d3231dda21056e92f30a87e
ianmcknnn <ianmcnnn@gmail.com>
2020-07-10 10:14:39 -0400

    added password confirmation functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
   def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
 
   validates :username, {presence: true, uniqueness: true}
   validates :password, {presence: true, length: { in: 6..20 }}
-  
+  validates_confirmation_of :password
+  validates_presence_of :password_confirmation, { case_sensitive: true}
+
+
   has_secure_password
 
 end
